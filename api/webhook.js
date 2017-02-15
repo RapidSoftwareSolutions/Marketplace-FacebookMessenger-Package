3c00c1d4478e5497ee5e90a5d19596fbb2e6d8d1
Spyes <lewis@rapidapi.com>
2017-02-15 16:24:02 +0200

    support multiple listens for same event

--- api/webhook.js
+++ api/webhook.js
@@ -3,19 +3,41 @@ const request = require('request');
 
 module.exports = (req, res) => {
     const {
-        body
+        params, body, headers
     } = req.body.args;
     const challenge = body["hub.challenge"];
-
-    const r = {
-        callback: 'success',
+    const token = body["hub.token"];
+    const resp = {
+        callback: '',
         contextWrites: {
-            to: JSON.stringify({
-                http_resp: challenge || '',
-                client_msg: body
-            })
+            to: ''
         }
     };
 
+    if (challenge || token) {
+        r.callback = 'success';
+        r.contextWrites.to = JSON.stringify({
+            http_resp: challenge || token,
+            client_msg: body,
+            socket_token: ''
+        });
+    } else {
+        const found = params.find(param => param.page_id === body.entry.id);
+        if (!found) {
+            r.callback = 'error';
+            r.contextWrites.to = JSON.stringify({
+                http_resp: '',
+                client_msg: 'Mismatching tokens',
+                socket_token: ''
+            });            
+        } else {
+            r.callback = 'success';
+            r.contextWrites.to = JSON.stringify({
+                http_resp: '',
+                client_msg: body,
+                socket_token: found._rapid_socket_token
+            });
+        }
+    }
     res.status(200).send(r);
 }
